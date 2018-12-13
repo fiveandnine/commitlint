e0120ee697c3468dbadf9a5238c47559a7e200b0
xiaolei <lei.xiao@ifchange.com>
2018-12-13 19:44:34 +0800

    test(): 测试完成commit message
     I just change string to test
        完整的commit message
        添加了一个 测试字符串
    
    Close #111

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'body-leading-blank': [1, 'always'],
     'footer-leading-blank': [1, 'always'],
     'header-max-length': [2, 'always', 72],
-    'header-min-length': [2, 'always', 20],
+    'header-min-length': [2, 'always', 10],
     'scope-case': [2, 'always', 'lower-case'],
     'subject-case': [
       2,
