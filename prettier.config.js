c66f08c7d352d452be2150f1683ca30e2a08d499
xiaolei <lei.xiao@ifchange.com>
2018-12-14 11:22:56 +0800

    test: delete eslintrc.js

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-        // tab缩进大小,默认为2
-        tabWidth: 8,
-        semi: false,
-        singleQuote: true,
-        arrowParens: 'avoid',
-        trailingComma: 'es5',
+  // tab缩进大小,默认为2
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'es5',
 }
